53f944732ebe216d63f41cafbb2d0c25b4fce87a
antonypico <e1315968907@live.uleam.edu.ec>
2025-10-25 13:44:58 -0500

    esto es una actualizacion del main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from forms.form_login import VentanaLogin
+from forms.form_master import VentanaPrincipal
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = VentanaLogin(root)
+    app = VentanaPrincipal(root)
     root.mainloop()
 
